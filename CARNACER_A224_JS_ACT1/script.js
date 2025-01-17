ed63523d80f860498d96555847ded19487771285
janexx-a11 <noemecarnacer26@gmail.com>
2025-01-17 18:12:46 +0800

    changes

---
+++ CARNACER_A224_JS_ACT1/script.js
@@ -0,0 +1,12 @@
+const btnCount = document.getElementById("btnCount")
+const txtCounter = document.getElementById("txtCounter");
+
+// lamba function
+let count = 0;
+btnCount.addEventListener("click", () => {
+    // logic
+    count = count + 1 // count++ // count += 1
+    txtCounter.textContent = count;
+    console.log(count)
+    console.log("Clicked")
+})
